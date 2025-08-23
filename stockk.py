86f5945ef446498fa6dd317154888d77115582bb
Abhinav Guleria <abhinavguleriaji22@gmail.com>
2025-08-23 17:30:44 +0530

    Delete stockk.py

--- stockk.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import seaborn as sns 
-import matplotlib.pyplot as plt
-import yfinance as yf
-stock = "AAPL"
-data = yf.download(stock,start = "2020-01-01", end = "2025-01-01")
-print(data.head())
